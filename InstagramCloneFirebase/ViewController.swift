2657b2f28c896f9fbebbb90ec3755a1384067cfb
Birkan <birkanpusa@icloud.com>
2021-12-24 18:27:33 +0300

    Initial Commit

---
+++ InstagramCloneFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramCloneFirebase
+//
+//  Created by Birkan Pusa on 24.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
