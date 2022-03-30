2b36cfd8da80cf1439d596daec9fd905660bca0c
Birkan <birkanpusa@icloud.com>
2022-03-30 18:09:17 +0300

    first commit

---
+++ InstagramCloneFirebase/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  InstagramCloneFirebase
+//
+//  Created by Birkan Pusa on 30.12.2021.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
